20a9e6d949dfefbc499a49b2f36b1c327bd891ef
“MiguelFor” <miguelfor@hotmail.com>
2019-02-08 13:12:28 -0500

    First commit

---
+++ data/movies.js
@@ -0,0 +1,99 @@
+var movies = [
+  {
+    "name": "The Shawshank Redemption",
+    "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "rating": 8.0,
+    "director": "Frank Darabont",
+    "year": 2020,
+    "stars": [
+      "Tim Robbins",
+      "Morgan Freeman",
+      "Bob Gunton"
+    ],
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Miguel"
+    ], 
+    "barrio": [
+      "Centro",
+      "Alamos"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": "The Godfather",
+    "outline": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    "rating": 9.2,
+    "director": "Francis Ford Coppola",
+    "year": 1972,
+    "stars": [
+      "Marlon Brando",
+      "Al Pacino",
+      "James Caan"
+    ],
+    "runtime": 175,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "barrio": [ 
+      "Alamos"
+    ],
+    "certificate": "R",
+    "date": "1974-07-12T10:29:29+05:30",
+    "actor": "Marlon Brando",
+    "id": 2
+  },
+  {
+    "name": "The Godfather: Part II",
+    "outline": "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.",
+    "rating": 9.1,
+    "director": "Francis Ford Coppola",
+    "year": 1974,
+    "stars": [
+      "Al Pacino",
+      "Robert De Niro",
+      "Robert Duvall"
+    ],
+    "runtime": 200,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "barrio": [ 
+      "Alamos"
+    ],
+    "certificate": "R",
+    "date": "1992-08-23T10:29:29+05:30",
+    "actor": "Al Pacino",
+    "id": 3
+  },
+  {
+    "name": "Pulp Fiction",
+    "outline": "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    "rating": 9.0,
+    "director": "Quentin Tarantino",
+    "year": 1994,
+    "stars": [
+      "John Travolta",
+      "Uma Thurman",
+      "Samuel L. Jackson"
+    ],
+    "runtime": 154,
+    "genre": [
+      "Crime",
+      "Thriller"
+    ],
+    "barrio": [ 
+      "Alamos"
+    ],
+    "certificate": "R",
+    "date": "1994-02-09T10:29:29+05:30",
+    "actor": "John Travolta",
+    "id": 4
+  }
+]
